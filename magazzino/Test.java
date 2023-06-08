7da0a4615264c0651f9befa33435abf7fc371921
EsterPassini <esterluce.passini@studenti.unimi.it>
2023-06-08 14:53:51 +0000

    sistemazione ultima cosina

--- magazzino/Test.java
+++ magazzino/Test.java
@@ -5,8 +5,6 @@ public class Test {
         RobotMicro robMicro = new RobotMicro();
         RobotExtra robEx = new RobotExtra();
 
-        //Scaffale s1 = new Scaffale();
-        //Scaffale s2 = new Scaffale();
 
         Pacco pa = new Pacco("banane", 3);
         Pacco pb = new Pacco("gattini", 2);
@@ -18,6 +16,9 @@ public class Test {
         System.out.println(m);
         System.out.println(m.spostaMag(1, 3, robMicro, 3));
         System.out.println(m);
-        
+        System.out.println("torniamo come prima: "+m.spostaMag(3, 1, robMicro, 3));
+        System.out.println(m);
+        System.out.println(m.spostaMag(1, 3, robEx, 3));
+        System.out.println(m);
     }
 }
